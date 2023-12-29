b27143689a7948ddc0ced059c4c0498966fbbf13
hyeon23 <urim2270@naver.com>
2023-12-29 15:04:34 +0900

    update

--- Assets/Scripts/TypeChangeStudy.cs
+++ Assets/Scripts/TypeChangeStudy.cs
@@ -7,7 +7,7 @@ public class TypeChangeStudy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Vector3
     }
 
     // Update is called once per frame
