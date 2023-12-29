49d8e9f3e6d910644375a1a201d5e3f9b1d04cb8
hyeon23 <urim2270@naver.com>
2023-12-29 22:13:36 +0900

    no message

--- Assets/Scripts/TypeChangeStudy.cs
+++ Assets/Scripts/TypeChangeStudy.cs
@@ -14,9 +14,23 @@ public class TypeChangeStudy : MonoBehaviour
         orc.Grr();
 
         //형변환 방법 (), is, as
-        if(orc is Monster)
+        if(orc is Monster) // orc is Monster : orc 객체가 Monster 자료형으로 변환 가능하냐?
         {
             monster = (Monster)orc;
+            monster.Grr();
+        }
+
+        monster = orc as Monster;
+
+        // as: ()와 기능이 유사 = (Monster)orc
+        //as는 형변환할 수 있는지 검사 후, 할 수 있으면 형변환 수행, 항 수 없다면 null을 대입해준다. = as 사용하는게 좋다.
+        //값 타입 = 소괄호 형번환 / 참조 타입 as 사용
+
+        //암시적 형변환도 가능
+        monster = orc;
+
+        if (monster != null)
+        {
             monster.Grr();
         }
     }
