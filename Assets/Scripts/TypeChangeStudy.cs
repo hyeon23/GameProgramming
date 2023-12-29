ce03bf2ac767bd28a457ceca727d70f414aa4976
hyeon23 <urim2270@naver.com>
2023-12-29 20:54:15 +0900

    no message

--- Assets/Scripts/TypeChangeStudy.cs
+++ Assets/Scripts/TypeChangeStudy.cs
@@ -7,7 +7,15 @@ public class TypeChangeStudy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Vector3
+        Debug.Log("이런 것들이 존재했지");
+    }
+
+    private void OnDestroy() {
+        Debug.Log("cout");
+    }
+
+    private void OnEnable() {
+        
     }
 
     // Update is called once per frame
