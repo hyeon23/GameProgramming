fb5c427a791e1e7980d01b5e7bbbeab73aa129bd
hyeon23 <urim2270@naver.com>
2023-12-31 02:01:54 +0900

    no message

--- Assets/Scripts/Array.cs
+++ Assets/Scripts/Array.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Array : MonoBehaviour
 {
@@ -25,4 +26,11 @@ public class Array : MonoBehaviour
         },
 
     };
+
+    Stack stk = new Stack();
+
+    private void Start()
+    {
+        
+    }
 }
