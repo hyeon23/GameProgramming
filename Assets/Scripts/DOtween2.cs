2b77be5147b53e88c823709fbe5d8e11128dea5d
hyeon23 <urim2270@naver.com>
2024-01-02 21:36:49 +0900

    DQ Study Update

--- Assets/Scripts/DOtween2.cs
+++ Assets/Scripts/DOtween2.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class DOtween2 : MonoBehaviour
 {
+    Vector3 targetPos = new Vector3(5, 5, 5);
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //transform.DOMove(targetPos, 5.0f);
+        //transform.DOMoveX(5, 5.0f);
+        //transform.DOMoveY(5, 5.0f);
+        //transform.DOMoveZ(5, 5.0f);
+        //transform.DOLocalMove(targetPos, 5.0f);
+        //transform.DOLocalMoveX(5, 5.0f);
+        //transform.DOLocalMoveY(5, 5.0f);
+        //transform.DOLocalMoveZ(5, 5.0f);
+        transform.DORotate(targetPos, 5.0f);
     }
 }
